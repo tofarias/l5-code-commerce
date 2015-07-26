edb7f839707c90c7b83f6b787dc0ee7a45cb6874
Tiago Farias <tiago.farias.poa@gmail.com>
2015-07-26 11:11:36 -0300

    Add - CRUD: criando formulário.

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -6,7 +6,21 @@
 	<h1>Create Categorie</h1>
 	
 	{!! Form::open() !!}
-	
+		
+		<div class="form-group">
+			{!! Form::label('name', 'Name:') !!}
+			{!! Form::text('name', null, ['class' => 'form-control']) !!}
+		 </div>
+		 
+		 <div class="form-group">
+			{!! Form::label('description', 'Description:') !!}
+			{!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+		 </div>
+		 
+		 <div class="form-group">
+		 {!! Form::submit('Add category,', ['class' => 'btn btn-primary form-control']) !!}
+		 </div>
+		
 	{!! Form::close() !!}
 	
 </div>
