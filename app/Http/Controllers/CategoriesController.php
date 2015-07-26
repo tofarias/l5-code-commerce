03914827beec4b4abafc56f8e05983692ad2f71e
Tiago Farias <tiago.farias.poa@gmail.com>
2015-07-26 10:55:35 -0300

    Add - CRUD: listando dados do model.

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,11 +6,21 @@ use Illuminate\Http\Request;
 
 use CodeCommerce\Http\Requests;
 use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\Category;
 
 class CategoriesController extends Controller
 {
+	private $categoryModel;
+	
+    
+    public function __construct(Category $categoryModel)
+    {
+    	$this->categoryModel = $categoryModel;
+    }
     public function index()
     {
-    	return view('categories.index');
+    	$categories = $this->categoryModel->all();
+    	
+    	return view('categories.index', compact('categories'));
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,5 +2,24 @@
 
 
 @section('content')
-<h1>Categories</h1>
+<div class="container">
+	<h1>Categories</h1>
+	
+	<table class="table">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Description</th>
+			<th>Action</th>
+		</tr>
+		@foreach( $categories as $category )
+		<tr>
+			<td>{{ $category->id }}</td>
+			<td>{{ $category->name }}</td>
+			<td>{{ $category->description }}</td>
+		</tr>
+		@endforeach
+		
+	</table>
+</div>
 @endsection
