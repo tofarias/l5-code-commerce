a3aca9f972f7b31627342916fbf7ed26c7850e57
Tiago Farias <tiago.farias.poa@gmail.com>
2015-07-30 19:14:18 -0300

    Add - Cart: criando classe Cart.

---
+++ app/Cart.php
@@ -0,0 +1,48 @@
+<?php namespace CodeCommerce;
+
+
+/**
+ * 
+ * @author tiago
+ *
+ */
+class Cart
+{
+	private $items;
+	
+	public function __construct()
+	{
+		$this->items = [];
+	}
+	
+	public function add($id, $name, $price)
+	{
+		$this->items + [
+				$id => [
+						'qtd' => isset( $this->items[$id]['qtd']) ? $this->items[$id]['qtd']++ : 1,
+						'price' => $price,
+						'name' => $name
+				]
+		];
+	}
+	
+	public function remove($id)
+	{
+		unset($this->items[$id]);
+	}
+	
+	public function all()
+	{
+		return $this->items;
+	}
+	
+	public function getTotal()
+	{
+		$total = 0;
+		foreach ($this->items as $items) {
+			$total += $items['qtd'] * $items['price'];
+		}
+		
+		return $total;
+	}
+}
